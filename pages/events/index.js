b57dc7896814a1052d26e5da2e312066cb96da01
mazyn <gilmarfilho75@gmail.com>
2022-04-22 16:52:20 -0300

    add filtered events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,24 @@
+import { useRouter } from "next/router";
+
 import { getAllEvents } from "../../data/dummy-data";
 import EventList from "../../components/events/event-list";
+import EventsSearch from "../../components/events/events-search";
 
 const EventsPage = () => {
+  const { push } = useRouter();
   const events = getAllEvents();
 
+  const findEventsHandler = (year, month) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    push(fullPath).then();
+  };
+
   return (
-    <div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </>
   );
 };
 
